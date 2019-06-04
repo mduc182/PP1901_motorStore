e709a54b4a110218ae0d8205274d7939106cd924
mduc182 <minhduc8120@gmail.com>
2019-06-04 08:50:16 +0700

    CreateMigration

---
+++ motorstore/database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=0;$i<=10;$i++)
+        {
+            DB::table('products')->insert(
+                [
+                    'pdname'=>Str::random(10),
+                    'plate'=>Str::random(10),
+                    'color'=>Str::random(10),
+                    'type'=>Str::random(10),
+                    'detail'=>Str::random(20),
+                    'year'=>rand(1,10),
+                    'price'=>rand(10,100),
+                    'created_at'=>date('Y-m-d H-i-s'),
+                    'updated_at'=>date('Y-m-d H-i-s'),
+
+                ]
+            );
+        }
+    }
+}
