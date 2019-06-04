e04eee65e9c54105692fd50d8b4e4a14fb10fce7
mduc182 <minhduc8120@gmail.com>
2019-06-04 08:50:16 +0700

    CreateMigration
    addfkey
    
    AddMigrationSeeder

---
+++ motorstore/database/seeds/BranchesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class BranchesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i <= 10; $i++) {
+            DB::table('branches')->insert(
+                [
+                    'address' => Str::random(10),
+                    'phone' => Str::random(10),
+
+                    'created_at' => date('Y-m-d H-i-s'),
+                    'updated_at' => date('Y-m-d H-i-s'),
+
+                ]
+            );
+        }
+    }
+}
---
+++ motorstore/database/seeds/OrderProductTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class OrderProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        {
+            {
+                for($i=0;$i<=5;$i++)
+                {
+                    DB::table('_order_product')->insert(
+                        [
+                           'order_id'=>rand(1,10),
+                           'product_id'=>rand(1,10),
+
+
+
+                            'created_at'=>date('Y-m-d H-i-s'),
+                            'updated_at'=>date('Y-m-d H-i-s'),
+
+                        ]
+                    );
+                }
+            }
+        }
+    }
+}
