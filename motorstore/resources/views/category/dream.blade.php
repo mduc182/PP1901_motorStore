0850c8b7a4f64b55db896b013c3a2060461874f4
mduc182 <minhduc8120@gmail.com>
2019-07-06 23:12:37 +0700

    UploadImage & Edit Product

--- motorstore/resources/views/category/dream.blade.php
+++ motorstore/resources/views/category/dream.blade.php
@@ -3,9 +3,7 @@
     @foreach($categories->products as $product)
         <div id="card" class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-                <a href="{!! Route('product_page', $product->id) !!}"><img class="card-img-top"
-                                 src="https://vn-test-11.slatic.net/p/8/xe-may-sh-mode-thoi-trang-trang-2017-smart-key-5673-7075394-12f70af9b6b57c7ce664c2966c941c82-catalog.jpg_340x340q80.jpg_.webp"
-                                 alt=""></a>
+                <a href="{!! Route('product_page', $product->id) !!}"><img src="/images/{!! $product->image !!}" alt="" width="150px" height="150px"></a>
                 <div class="card-body">
                     <h4 class="card-title">
                         <a href="{!! Route('product_page', $product->id) !!}">{{ trans('messages.pdname')}}{!! $product -> pdname !!}</a>
